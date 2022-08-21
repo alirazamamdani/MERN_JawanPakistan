89a6950d3d79cf4c74c011def758d8dfbf02cc71
alirazamamdani <Ali.mamdani@hotmail.com>
2022-08-21 18:05:24 +0500

    learning react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+// import { Hello } from "./Hello";
 
 function App() {
+  const [currentText, setCurrentText] = useState("");
+
+  const [todo, setTodo] = useState([]);
+
+  const addTodo = () => {
+    setTodo([...todo, currentText]);
+    setCurrentText("");
+  };
+  const deleteTodo = (i) => {
+    todo.splice(i, 1);
+    setTodo([...todo]);
+  };
+  const editTodo = (i) => {
+    todo[i] = prompt("Enter Update Value", todo[i]);
+    setTodo([...todo]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          type="text"
+          value={currentText}
+          placeholder="Enter Something here"
+          onChange={(e) => setCurrentText(e.target.value)}
+        />
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={addTodo}>
+          Add Todo
+        </button>
+      </div>
+      <div>
+        {todo.map((curr, i) => {
+          return (
+            <ul key={i}>
+              <li>
+                {curr}{" "}
+                <button
+                  className="btn btn-success"
+                  onClick={() => {
+                    editTodo(i);
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  className="btn btn-danger mx-2"
+                  onClick={() => deleteTodo(i)}
+                >
+                  Delete
+                </button>
+              </li>
+            </ul>
+          );
+        })}
+      </div>
     </div>
   );
 }
